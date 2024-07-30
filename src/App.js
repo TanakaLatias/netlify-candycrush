7fdd9372b64aeea8cf277e3c9463b770af38804f
Sawa <sawa@sawa.com>
2024-07-30 21:53:50 +0900

    fixed to deploy2

--- src/App.js
+++ src/App.js
@@ -23,14 +23,30 @@ const App = () => {
   const [score, setScore] = useState(0);
   const [beBlank, setBeBlank] = useState([]);
 
+  const [five1, setFive1] = useState(false);
+  const [five2, setFive2] = useState(false);
+  const [four1, setFour1] = useState(false);
+  const [four2, setFour2] = useState(false);
+  const [three1, setThree1] = useState(false);
+  const [three2, setThree2] = useState(false);
+
   const check = useCallback(() => {
     const willBeBlank = [];
     
+    for (let i = 0; i < width * (width - 4); i++) {
+      const currentIs = [i, i + width, i + width * 2, i + width * 3, i + width * 4];
+      if (!currentBoard[i] || currentBoard[i] === blank) continue;
+      if (currentIs.every(ci => currentBoard[ci] === currentBoard[i])) {
+        setFive1(true);
+        currentIs.forEach(ci => willBeBlank.push(ci));
+      }
+    }
+
     for (let i = 0; i < width * (width - 3); i++) {
       const currentIs = [i, i + width, i + width * 2, i + width * 3];
       if (!currentBoard[i] || currentBoard[i] === blank) continue;
       if (currentIs.every(ci => currentBoard[ci] === currentBoard[i])) {
-        setScore(prevScore => prevScore + 4);
+        setFour1(true);
         currentIs.forEach(ci => willBeBlank.push(ci));
       }
     }
@@ -39,32 +55,65 @@ const App = () => {
       const currentIs = [i, i + width, i + width * 2];
       if (!currentBoard[i] || currentBoard[i] === blank) continue;
       if (currentIs.every(ci => currentBoard[ci] === currentBoard[i])) {
-        setScore(prevScore => prevScore + 3);
+        setThree1(true);
         currentIs.forEach(ci => willBeBlank.push(ci));
       }
     }
     
+    const invalidIs0 = [4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31, 36, 37, 38, 39, 44, 45, 46, 47, 52, 53, 54, 55, 60, 61, 62, 63];
+    for (let i = 0; i < width * width - 4; i++) {
+      const currentIs = [i, i + 1, i + 2, i + 3, i + 4];
+      if (invalidIs0.includes(i) || !currentBoard[i] || currentBoard[i] === blank) continue;
+      if (currentIs.every(ci => currentBoard[ci] === currentBoard[i])) {
+        setFive2(true);
+        currentIs.forEach(ci => willBeBlank.push(ci));
+      }
+    }
+
     const invalidIs = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 61, 62, 63];
-    
     for (let i = 0; i < width * width - 3; i++) {
       const currentIs = [i, i + 1, i + 2, i + 3];
       if (invalidIs.includes(i) || !currentBoard[i] || currentBoard[i] === blank) continue;
       if (currentIs.every(ci => currentBoard[ci] === currentBoard[i])) {
-        setScore(prevScore => prevScore + 4);
+        setFour2(true);
         currentIs.forEach(ci => willBeBlank.push(ci));
       }
     }
     
     const invalidIs2 = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63];
-    
     for (let i = 0; i < width * width - 2; i++) {
       const currentIs = [i, i + 1, i + 2];
       if (invalidIs2.includes(i) || !currentBoard[i] || currentBoard[i] === blank) continue;
       if (currentIs.every(ci => currentBoard[ci] === currentBoard[i])) {
-        setScore(prevScore => prevScore + 3);
+        setThree2(true);
         currentIs.forEach(ci => willBeBlank.push(ci));
       }
     }
+
+    if (five1) {
+      setScore(prevScore => prevScore + 1);
+      setFive1(false);
+    };
+    if (four1) {
+      setScore(prevScore => prevScore + 1);
+      setFour1(false);
+    };
+    if (three1) {
+      setScore(prevScore => prevScore + 3);
+      setThree1(false);
+    };
+    if (five2) {
+      setScore(prevScore => prevScore + 1);
+      setFive2(false);
+    };
+    if (four2) {
+      setScore(prevScore => prevScore + 1);
+      setFour2(false);
+    };
+    if (three2) {
+      setScore(prevScore => prevScore + 3);
+      setThree2(false);
+    };
   
     if (willBeBlank.length > 0) {
       setBeBlank(willBeBlank);
@@ -112,17 +161,16 @@ const App = () => {
       const validIds = [currentNumber-1, currentNumber+1, currentNumber-width, currentNumber+width];
       const validityOne = validIds.includes(nextNumber);
       const validityTwo = nextSquare.getAttribute('src') !== blank;
-      const validityThree = check()
 
       currentBoard[currentNumber] = nextSquare.getAttribute('src');
       currentBoard[nextNumber] = currentSquare.getAttribute('src');
       
       if (validityOne&&validityTwo) {
-        console.log("valid", nextNumber, validityOne, validityTwo, validityThree);
+        console.log("valid", nextNumber, validityOne, validityTwo);
         setCurrentSquare(null);
         setNextSquare(null);
       } else {
-        console.log("invalid", nextNumber, validityOne, validityTwo, validityThree);
+        console.log("invalid", nextNumber, validityOne, validityTwo);
         console.log(validIds);
         currentBoard[currentNumber] = currentSquare.getAttribute('src');
         currentBoard[nextNumber] = nextSquare.getAttribute('src');
